007ea534d589589884a92355d75bd36f4b764dad
Alexis Boggan <alexisboggan@gmail.com>
2021-04-19 07:31:06 -0300

    first mock json

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,77 @@
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' })
+    const menuData = {
+        topLevel: [
+          {
+            name: "Comunidad \n Errepar",
+            id: 101,
+            children: [
+              {
+                name: "conozca errepar",
+                id: 1001,
+                link: "conozca",
+              },
+              {
+                name: "quienes somos",
+                id: 1002,
+                link: "quienessomos",
+              },
+              {
+                name: "trabajar en errepar",
+                id: 1111,
+                link: "trabajar",
+              },
+            ],
+          },
+          {
+            name: "productos \n servicios",
+            id: 102,
+            children: [
+              {
+                name: "aplicaciones",
+                id: 1003,
+                link: "aplicaciones",
+              },
+              {
+                name: "subscripciones",
+                id: 1004,
+                link: "subscripciones",
+              },
+            ],
+          },
+          {
+            name: "subscripciones",
+            id: 103,
+            children: [
+              {
+                name: "conozca errepar",
+                id: 1005,
+                link: "conozca",
+              },
+              {
+                name: "quienes somos",
+                id: 1006,
+                link: "quienessomos",
+              },
+            ],
+          },
+          {
+            name: "contacto",
+            id: 105,
+            children: [
+              {
+                name: "aplicaciones",
+                id: 1034,
+                link: "aplicaciones",
+              },
+              {
+                name: "subscripciones",
+                id: 1023,
+                link: "subscripciones",
+              },
+            ],
+          },
+        ],
+      };
+    res.status(200).json(menuData);
   }
   
